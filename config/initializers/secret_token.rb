36d65d4a30c689ba621817c5f7c7bbcb3b02fb5c
Dickson S. Guedes <guedes@guedesoft.net>
2011-02-14 23:16:58 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ganesha::Application.config.secret_token = '915ee0897202c4d326dd7ea7093913987021e70eeed06edf83f7ad16657db0c15dc120cf2daf367efa50dc51db3016dad194811579430fc89597baa0bc8c6c84'
