535fda6439a38857d9bc6568b34d466c99c738f9
Dickson S. Guedes <guedes@guedesoft.net>
2011-03-13 23:28:07 -0300

    removing some unused fields

--- app/controllers/data_collectors_controller.rb
+++ app/controllers/data_collectors_controller.rb
@@ -1,83 +1,44 @@
 class DataCollectorsController < ApplicationController
-  # GET /data_collectors
-  # GET /data_collectors.xml
+  respond_to :html, :js, :json, :xml
+
   def index
-    @data_collectors = DataCollector.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @data_collectors }
-    end
-  end
-
-  # GET /data_collectors/1
-  # GET /data_collectors/1.xml
-  def show
-    @data_collector = DataCollector.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @data_collector }
-    end
-  end
-
-  # GET /data_collectors/new
-  # GET /data_collectors/new.xml
-  def new
     @data_collector = DataCollector.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @data_collector }
-    end
+    respond_with @data_collectors = DataCollector.all
   end
 
-  # GET /data_collectors/1/edit
+  def show
+    respond_with(@data_collector = DataCollector.find(params[:id]))
+  end
+
+  def new
+    respond_with(@data_collector = DataCollector.new)
+  end
+
   def edit
-    @data_collector = DataCollector.find(params[:id])
+    respond_with(@data_collector = DataCollector.find(params[:id]))
   end
 
-  # POST /data_collectors
-  # POST /data_collectors.xml
   def create
     @data_collector = DataCollector.new(params[:data_collector])
-
-    respond_to do |format|
-      if @data_collector.save
-        format.html { redirect_to(@data_collector, :notice => 'Data collector was successfully created.') }
-        format.xml  { render :xml => @data_collector, :status => :created, :location => @data_collector }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @data_collector.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Data collector was successfully created.' if @data_collector.save
+    respond_with @data_collector
   end
 
-  # PUT /data_collectors/1
-  # PUT /data_collectors/1.xml
   def update
-    @data_collector = DataCollector.find(params[:id])
-
-    respond_to do |format|
-      if @data_collector.update_attributes(params[:data_collector])
-        format.html { redirect_to(@data_collector, :notice => 'Data collector was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @data_collector.errors, :status => :unprocessable_entity }
-      end
+    @data_collector = DataCollector.new(params[:data_collector])
+    if @data_collector.update_attributes(params[:data_collector])
+      flash[:notice] = 'Data collector was successfully updated.'
     end
+
+    respond_with @data_collector
   end
 
-  # DELETE /data_collectors/1
-  # DELETE /data_collectors/1.xml
   def destroy
     @data_collector = DataCollector.find(params[:id])
     @data_collector.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(data_collectors_url) }
-      format.xml  { head :ok }
-    end
+    flash[:notice] = 'Data collector was successfully destroyed.'
+
+    respond_with @data_collector
   end
 end
