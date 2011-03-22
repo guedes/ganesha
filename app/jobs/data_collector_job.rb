bec13ee6dbaf7e15dffe424602c561a1d0d1b180
Dickson S. Guedes <guedes@guedesoft.net>
2011-03-22 08:16:55 -0300

    WIP: data collector now execute the script on target

--- app/jobs/data_collector_job.rb
+++ app/jobs/data_collector_job.rb
@@ -1,9 +1,41 @@
+class TargetDatabase < ActiveRecord::Base
+end
+
 class DataCollectorJob
+  
   @queue = :data_collector
   
   def self.perform(script_target_id)
-    puts "Executing #{ScriptTarget.find(script_target_id).name}"
-    sleep 3
-    puts "Executed"
+    st = ScriptTarget.find(script_target_id)
+    if st.script.type == ScriptType::SQL
+      puts "Executing #{st.name}"
+      a = self.select(st)
+      puts a.to_yaml
+      puts "Executed"
+    end
   end
+
+  private
+    def self.select(st)
+      self.connection(self.build_options(st)).select_all(st.script.content)
+    end
+
+    def self.connection(options)
+      TargetDatabase.establish_connection(options).connection
+    end
+
+    def self.build_options(script_target)
+      database = script_target.targetable
+      instance = database.instance
+      server   = instance.server
+
+      options={
+      :adapter  => "postgresql",
+      :database => database.name,
+      :port     => instance.port,
+      :host     => server.host_address,
+      :username => "monitor",
+      :password => "monitor"
+      }
+    end
 end
